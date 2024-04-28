9e3068e7613f8c9f6d2d468ed58d900ce2f54bff
lambda <lambda@fureverfoster.com>
2024-04-28 14:36:33 -0400

    changed main page that loads on start up to have a list of vue tutorial item and updated README.md

---
+++ vue-features/src/components/ReactivityFundamentals.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+//<p>Additional Ref Unwrapping Details</p>
+const refObj = ref('origRefValue')
+const reactiveObj = reactive({
+	refObj
+})
+
+reactiveObj.refObj = 'newRefValue'
+</script>
+
+<template>
+	<div class="greetings">
+		<h2>ID:</h2>
+		<p>75fe51f9c6760084f3460cefc5d77c2e</p>
+		<h2>Topic:</h2>
+		<p>Additional Ref Unwrapping Details</p>
+		<h2>Description:</h2>
+		<p>Ref automatically unwrapped when access/mutated as property of reactive object</p>
+		<h2>Code:</h2>
+		<pre>
+		  <code>
+		  import { ref, reactive } from 'vue'
+
+		  const refObj = ref('origRefValue')
+		  const reactiveObj = reactive({
+			  refObj
+		  })
+
+		  reactiveObj.refObj = 'newRefValue'
+		  </code>
+		  </pre>
+
+		<h2>Output:</h2>
+		<div>{{ refObj }}</div>
+	</div>
+</template>
+
+<style scoped>
+h2 {
+	font-weight: bold;
+	font-size: 2rem;
+}
+</style>
